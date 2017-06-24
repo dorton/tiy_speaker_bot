138e5036ef746c50ea103498ffc9ba9496c19780
Brian Dorton <dorton@gmail.com>
2017-06-23 20:54:10 -0500

    adds an error handle for when date is null

--- src/index.js
+++ src/index.js
@@ -1,22 +1,16 @@
 const patFacts = require('./patFacts');
-
-
-
 var Alexa = require('alexa-sdk');
 var http = require('http');
-
 var welcomeRepromt = "You can ask me about a guest speaker at the iron yard. What will it be?";
 var HelpMessage = "Here are some things you  can say: Who is speaking this week. Who is speaking today. Who spoke last week. Tell me about the speaker this week. What would you like to do?";
 var moreInformation = "See your Alexa app for  more  information."
 var tryAgainMessage = "please try again."
 var noSpeakerErrorMessage = "There was an error finding this speaker, " + tryAgainMessage;
 var goodbyeMessage = "Goodbye"
-var newline = "\n";
 var states = {
     SEARCHMODE: '_SEARCHMODE',
 };
 var output = "";
-
 var newSessionHandlers = {
     'LaunchRequest': function () {
         this.handler.state = states.SEARCHMODE;
@@ -46,20 +40,14 @@ var newSessionHandlers = {
         this.emit(':ask', output, welcomeRepromt);
     },
 };
-
 var handlers = Alexa.CreateStateHandler(states.SEARCHMODE, {
-
     'getSpeakerIntent': function () {
+      var noDate = "Sorry, I did not hear the dates you wanted to check."
       var patFact = patFacts[Math.floor(patFacts.length * Math.random())];
-
-
       var alexa = this;
-
-        var date = this.event.request.intent.slots.date.value;
-
-
+      var date = this.event.request.intent.slots.date.value;
+      if (date != ""){
         httpGet(date, function (response) {
-
             // Parse the response into a JSON object ready to be formatted.
             var responseData = JSON.parse(response);
             var cardContent = "Data provided by DorkTron\n\n";
@@ -78,25 +66,22 @@ var handlers = Alexa.CreateStateHandler(states.SEARCHMODE, {
               else {
                 output = multipleSpeakerMessage;
               }
-
               responseData.forEach( function(talk, index){
                 var speaker = talk.speaker;
-
                 output += " Speaker " + (index+1) + ": " + speaker.name + ";";
-
                 cardContent += " Speaker " + (index+1) + ".\n";
                 cardContent += speaker.name + ".\n\n";
-
               })
               output += patFact;
             }
-
             var cardTitle = "The Iron Yard Guest Speakers";
-
             alexa.emit(':tellWithCard', output, cardTitle, cardContent);
         });
+      }
+      else {
+        alexa.emit(':ask', noDate, HelpMessage);
+      }
     },
-
     'getMeetupIntent': function () {
 
       var alexa = this;
@@ -142,30 +127,27 @@ var handlers = Alexa.CreateStateHandler(states.SEARCHMODE, {
             alexa.emit(':tellWithCard', output, cardTitle, cardContent);
         });
     },
-
     'AMAZON.HelpIntent': function () {
         output = HelpMessage;
         this.emit(':ask', output, HelpMessage);
     },
-
     'AMAZON.StopIntent': function () {
         this.emit(':tell', goodbyeMessage);
     },
-
     'AMAZON.CancelIntent': function () {
         // Use this function to clear up and save any data needed between sessions
         this.emit(":tell", goodbyeMessage);
     },
-
+    'Unhandled': function () {
+        output = HelpMessage;
+        this.emit(':ask', output, welcomeRepromt);
+    },
 });
-
-
 exports.handler = function (event, context, callback) {
     var alexa = Alexa.handler(event, context);
     alexa.registerHandlers(newSessionHandlers, handlers);
     alexa.execute();
 };
-
 // Create a web request and handle the response.
 function httpGet(date, callback) {
 
