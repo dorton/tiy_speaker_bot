a2c3971de472782442eba749f10f1095eab897ca
Brian Dorton <dorton@gmail.com>
2017-06-23 19:51:39 -0500

    updates skills

---
+++ src/patFacts.js
@@ -0,0 +1,13 @@
+const patFacts = [
+  "also, as an F. Y. I. Patrick is a certified lifeguard, N. B. D.",
+  "also, as an F. Y. I. Patrick is a former fire fighter, N. B. D.",
+  "also, as an F. Y. I. Patrick is an avid bike rider, N. B. D.",
+  "also, as an F. Y. I. Patrick was an R. E. I. executive at one point, N. B. D.",
+  "also, as an F. Y. I. Patrick built a store that allows users to buy and sell bikes, N. B. D.",
+  "also, as an F. Y. I. Patrick is learning functional programming, N. B. D.",
+  "also, as an F. Y. I. Patrick is is pretty much an expert in all things A. W. S., N. B. D.",
+  "also, as an F. Y. I. Patrick closely resembles Jeff Bezos in both drive and innovation, N. B. D."
+];
+
+
+module.exports = patFacts;
